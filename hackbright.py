50d160fd9f08a28885ad37a6b7c9e5f67ac934cc
Khanh Mai <khanhmai@creighton.edu>
2021-05-25 20:04:44 +0000

    initial commit
    co-authored-by: Nicole Sanchez <njbsanchez@gmail.com>

---
+++ hackbright.py
@@ -0,0 +1,99 @@
+"""Hackbright Project Tracker.
+
+A front-end for a database that allows users to work with students, class
+projects, and the grades students receive in class projects.
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy()
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///hackbright'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+def get_student_by_github(github):
+    """Given a GitHub account name, print info about the matching student."""
+
+    QUERY = """
+        SELECT first_name, last_name, github
+        FROM students
+        WHERE github = :github
+        """
+
+    db_cursor = db.session.execute(QUERY, {'github': github})
+
+    row = db_cursor.fetchone()
+
+    print("Student: {} {}\nGitHub account: {}".format(row[0], row[1], row[2]))
+
+
+def make_new_student(first_name, last_name, github):
+    """Add a new student and print confirmation.
+
+    Given a first name, last name, and GitHub account, add student to the
+    database and print a confirmation message.
+    """
+    pass
+
+
+def get_project_by_title(title):
+    """Given a project title, print information about the project."""
+    pass
+
+
+def get_grade_by_github_title(github, title):
+    """Print grade student received for a project."""
+    pass
+
+
+def assign_grade(github, title, grade):
+    """Assign a student a grade on an assignment and print a confirmation."""
+    pass
+
+
+def handle_input():
+    """Main loop.
+
+    Repeatedly prompt for commands, performing them, until 'quit' is received
+    as a command.
+    """
+
+    command = None
+
+    while command != "quit":
+        input_string = input("HBA Database> ")
+        tokens = input_string.split()
+        command = tokens[0]
+        args = tokens[1:]
+
+        if command == "student":
+            github = args[0]
+            get_student_by_github(github)
+
+        elif command == "new_student":
+            first_name, last_name, github = args  # unpack!
+            make_new_student(first_name, last_name, github)
+
+        else:
+            if command != "quit":
+                print("Invalid Entry. Try again.")
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # handle_input()
+
+    # To be tidy, we close our database connection -- though,
+    # since this is where our program ends, we'd quit anyway.
+
+    db.session.close()
